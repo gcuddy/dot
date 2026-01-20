418551d0ab87d6ee1c649d1770a7d14592359f28
Gus Cuddy <gus@coverbase.ai>
2026-01-20 17:41:46 -0500

    add config new to show untracked files in tracked dirs

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -4,6 +4,19 @@
 function config
     if test "$argv[1]" = "lg"
         lazygit --git-dir=$HOME/.dot --work-tree=$HOME
+    else if test "$argv[1]" = "new"
+        # Show new (untracked) files in tracked directories
+        # Usage: config new [directory]  (e.g., config new themes)
+        if test (count $argv) -gt 1
+            # Check specific directory
+            cd $HOME && /usr/bin/git --git-dir=$HOME/.dot --work-tree=$HOME ls-files --others --exclude-standard .config/$argv[2]/
+        else
+            # Check core tracked directories only
+            set -l core_dirs fish nvim tmux ghostty lazygit git bat btop aerospace karabiner atuin themes dark-notify starship.toml amp gh gh-dash mise
+            for dir in $core_dirs
+                cd $HOME && /usr/bin/git --git-dir=$HOME/.dot --work-tree=$HOME ls-files --others --exclude-standard .config/$dir 2>/dev/null
+            end
+        end
     else if test "$argv[1]" = "untracked"
         # Show top-level untracked items in .config (must run from $HOME)
         set -l tracked
